50abfc82be268b963f3444218e178add194ed9d3
ViPoSoft <viposoft@yandex.ru>
2016-11-13 15:12:37 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 def app(environ, start_response):
-    data = ''
-    for line in environ["QUERY_STRING"].split("&"):
-        data = data+line+"\n"
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-return data
+#    data = ''
+#    for line in environ["QUERY_STRING"].split("&"):
+#        data = data+line+"\n"
+#    start_response('200 OK', [('Content-Type', 'text/plain')])
+#return data
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
