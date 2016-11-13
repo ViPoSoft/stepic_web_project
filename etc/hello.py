425f6ede3bfe241dd5c3f7df44a1842729521df7
ViPoSoft <viposoft@yandex.ru>
2016-11-13 15:49:06 +0200

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,10 +1,14 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--workers=4',
-        '--timeout=60',
-        'hello:app'
-    )
-}
+#CONFIG = {
+#    'mode': 'wsgi',
+#    'working_dir': '/home/box/web',
+#    'args': (
+#        '--bind=0.0.0.0:8080',
+#        '--workers=4',
+#        '--timeout=60',
+#        'hello:app'
+#    )
+#}
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
+workers = 4
+#
