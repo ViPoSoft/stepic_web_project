08f850f0ae0cae9be2913bead9cff28184d24c4b
ViPoSoft <viposoft@yandex.ru>
2016-11-13 17:04:43 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s  /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/gunic_dj.conf   /etc/gunicorn.d/test_dj
 #1.9 Запуск WSGI приложений
-#sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo -s ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
