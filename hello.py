7ac77127c03447caed2d26e931028b3963ec7fbe
ViPoSoft <viposoft@yandex.ru>
2016-11-14 02:31:08 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,36 +1,18 @@
-#def app(environ, start_response):
-#    data = ''
-#    for line in environ["QUERY_STRING"].split("&"):
-#        data = data+line+"\n"
-#    start_response('200 OK', [('Content-Type', 'text/plain')])
-#return data
-
-
-#    data = '\n'.join(environ['QUERY_STRING'].split('&'))
-#    status = '200 OK'
-#    response_headers = [
-#        ('Content-type','text/plain'),
-#        ('Content-Length', str(len(data)))
-#    ]
-#    start_response(status, response_headers)
-#    return iter([data])
-#
-from re import sub
-
 def app(environ, start_response):
-    """wsgi minimal app.
-    $ gunicorn hello:app
-    """
-    #parameters = parse_qs(environ.get('QUERY_STRING', ''))
 
-    #output = ''
-    #for p in parameters:
-    #    for r in parameters[p]:
-    #        output += p + '=' + r + '\n'
-    #start_response('200 OK', [('Content-Type', 'text/plain')])
+	
+	raw_uri = str(environ.get('RAW_URI'))
+	
+	raw_uri = raw_uri[2:]
 
-    #return iter([str.encode(output)])
-    output = sub('&', '\n', environ.get('QUERY_STRING', ''))
-
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return iter([str.encode(output)])
+	params = raw_uri.split('&')
+	
+	data = ''
+	for param in params:
+		data += param + '\r\n'
+	
+	start_response("200 OK", [
+	  ("Content-Type", "text/plain"),
+	  ("Content-Length", str(len(data)))
+	])
+	return iter([data])
