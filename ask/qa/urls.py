e63a9d950423a0571d128c0bff7cf6862dd65538
ViPoSoft <viposoft@yandex.ru>
2016-11-16 01:50:35 +0200

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,18 +1,18 @@
 #from django.conf.urls import patterns, include, url
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', views.index, name='index'),
-    url(r'^popular/.*$', views.popular, name='popular'),    
+    ##Examples:
+    #url(r'^$', views.index, name='index'),
+    #url(r'^popular/.*$', views.popular, name='popular'),    
     url(r'^ask/.*$', views.ask, name='ask'),
-    url(r'^answer/.*$', views.answer, name='answer'),
-    url(r'^signup/.*$', views.user_signup, name='signup'),
-    url(r'^login/.*$', views.user_login, name='login'),
-    url(r'^logout/.*$', views.user_logout, name='logout'),    
-    url(r'^new/.*$', views.test), 
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^answer/.*$', views.answer, name='answer'),
+    #url(r'^signup/.*$', views.user_signup, name='signup'),
+    #url(r'^login/.*$', views.user_login, name='login'),
+    #url(r'^logout/.*$', views.user_logout, name='logout'),    
+    #url(r'^new/.*$', views.test), 
+    ## url(r'^$', 'ask.views.home', name='home'),
+    ## url(r'^blog/', include('blog.urls')),
     url(r'^(?P<question_id>[0-9]+)/$', views.test, name='test'),
 ]	
