571c642bfae33e3d0d9a6396664e862812d36ac4
ViPoSoft <viposoft@yandex.ru>
2016-11-16 19:04:21 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 
-sudo rm /etc/nginx/sites-enabled/test.conf
+#sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
