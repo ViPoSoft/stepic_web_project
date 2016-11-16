ce8964ec551f4b8eb397b648060d25fa0c479cc5
ViPoSoft <viposoft@yandex.ru>
2016-11-17 01:02:12 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,15 +3,19 @@
 # symbolic link to nginx config
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
-#sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+
 sudo /etc/init.d/nginx restart
 
 # symbolic links to gunicorn configs
 sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django-gunicorn.conf /etc/gunicorn.d/django-gunicorn.conf
 
+sudo unlink /etc/gunicorn.d/django
+sudo ln -s /home/box/web/etc/django /etc/gunicorn.d/django
+
 # run gunicorn server
-sudo gunicorn -с /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn  /etc/gunicorn.d/hello.py hello:app
 sudo gunicorn -с /etc/gunicorn.d/django-gunicorn.conf ask.wsgi:application
 
 #sudo /etc/init.d/gunicorn restart
