ffdcf7bd23dad2129f17b93dcf6de6ecceea9803
ViPoSoft <viposoft@yandex.ru>
2016-11-17 01:50:36 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ sudo unlink /etc/gunicorn.d/django
 sudo ln -s /home/box/web/etc/django /etc/gunicorn.d/django
 
 # run gunicorn server
-sudo gunicorn -u nobody /etc/gunicorn.d/hello.py hello:app
-sudo gunicorn -u nobody /etc/gunicorn.d/django-gunicorn.conf ask.wsgi:application
+#sudo gunicorn -u nobody /etc/gunicorn.d/hello.py hello:app
+#sudo gunicorn -u nobody /etc/gunicorn.d/django-gunicorn.conf ask.wsgi:application
 
 sudo /etc/init.d/gunicorn restart
