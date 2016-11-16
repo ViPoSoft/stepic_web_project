f6308195faba109d64e611e4c49f0c751d73c8ac
ViPoSoft <viposoft@yandex.ru>
2016-11-16 23:54:52 +0200

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -14,5 +14,12 @@ urlpatterns = [
     #url(r'^new/.*$', views.test), 
     ## url(r'^$', 'ask.views.home', name='home'),
     ## url(r'^blog/', include('blog.urls')),
+    # druga sproba
+    url(r'^$', views.test, name='test'), 
+    url(r'^login/.*$', views.test),
+    url(r'^signup/.*$', views.test),
+    url(r'^ask/.*$', views.test),
+    url(r'^popular/.*$', views.test),
+    url(r'^new/.*$', views.test),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
 ]	
