50c266ac0b95793ec9f532a627683067bfbe26b6
ViPoSoft <viposoft@yandex.ru>
2016-11-18 02:01:39 +0200

    les 2.1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,25 +1,24 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from . import views
+#from django.contrib import admin
+#from . import views
+from qa.views import test
 
-urlpatterns = [
-    ##Examples:
-    #url(r'^$', views.index, name='index'),
-    #url(r'^popular/.*$', views.popular, name='popular'),    
-    #url(r'^ask/.*$', views.test, name='ask'),
-    #url(r'^answer/.*$', views.answer, name='answer'),
-    #url(r'^signup/.*$', views.user_signup, name='signup'),
-    #url(r'^login/.*$', views.user_login, name='login'),
-    #url(r'^logout/.*$', views.user_logout, name='logout'),    
-    #url(r'^new/.*$', views.test), 
-    ## url(r'^$', 'ask.views.home', name='home'),
-    ## url(r'^blog/', include('blog.urls')),
-    # druga sproba
-    url(r'^$', views.test, name='test'), 
-    url(r'^login/.*$', views.test),
-    url(r'^signup/.*$', views.test),
-    url(r'^ask/.*$', views.test),
-    url(r'^popular/.*$', views.test),
-    url(r'^new/.*$', views.test),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
-]	
+urlpatterns = patterns('',
+    url(r'^$', test, name='test'),
+    url(r'^login/', test, name='login'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^question/\d+/', test, name='question'),
+    url(r'^ask/', test, name='ask'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^new/', test, name='new'),
+)
+#urlpatterns = [
+    ## druga sproba
+    #url(r'^$', views.test, name='test'), 
+    #url(r'^login/.*$', views.test),
+    #url(r'^signup/.*$', views.test),
+    #url(r'^ask/.*$', views.test),
+    #url(r'^popular/.*$', views.test),
+    #url(r'^new/.*$', views.test),
+    #url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
+#]	
