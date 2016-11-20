51c394ab29460417939c5b8f4eb38e6f76ab3625
ViPoSoft <viposoft@yandex.ru>
2016-11-20 03:41:28 +0200

    les 2.3   v1

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #  'qa',
+    'qa',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,12 +56,23 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'base23',
+        'USER': 'vipo',
+        'PASSWORD': 'vps23',
+        #'HOST': '127.0.0.1',
+        #'PORT': '3306',
+        }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
