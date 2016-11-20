90a2d873154d90ce04b3f3106ebd328c0a5fdc4a
ViPoSoft <viposoft@yandex.ru>
2016-11-20 12:11:13 +0200

    les 2.3  v1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# менеджер модели Question
+class QuestionManager(models.Manager):
+    def new(self):
+        from django.db import connection
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM qa_question q ORDER BY q.added_at DESC")
+        last_questions = cursor.fetchall()
+        return last_questions
+
+    def popular(self):
+        from django.db import connection
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM qa_question q ORDER BY q.rating DESC")
+        popular_questions = cursor.fetchall()
+        return popular_questions
+
+# определяю модели
+
+class Question(models.Model):
+    title = models.CharField(max_length=125)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    likes = models.TextField()
+    
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    
