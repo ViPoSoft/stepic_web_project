1c02a139be9ce357b6d6c74977a151e7fa2a42c2
ViPoSoft <viposoft@yandex.ru>
2016-11-20 14:16:57 +0200

    les 2.3 v2

--- init23.sh
+++ init23.sh
@@ -1,5 +1,7 @@
-sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE base23;"
+sudo /etc/init.d/mysql restart
+mysql -uroot -e "CREATE DATABASE base23 CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -uroot -e "CREATE USER 'vipo@localhost' IDENTIFIED BY 'vps23';"
-mysql -uroot -e "GRANT ALL ON base23.* TO 'vipo@localhost';"
+mysql -uroot -e "GRANT ALL ON base23.* TO 'vipo@localhost' IDENTIFIED BY 'vps23';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
+
+python /home/box/web/ask/manage.py syncdb
