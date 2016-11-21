642f2f3babd14e798b970de99d35c79fea8d35e5
ViPoSoft <viposoft@yandex.ru>
2016-11-21 21:46:46 +0200

    les 25 v1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,35 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class QuestionManager(models.Manager):
-    def new(self):
-        from django.db import connection
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM qa_question q ORDER BY q.added_at DESC")
-        last_questions = cursor.fetchall()
-        return last_questions
-
-    def popular(self):
-        from django.db import connection
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM qa_question q ORDER BY q.rating DESC")
-        popular_questions = cursor.fetchall()
-        return popular_questions
-
 
 class Question(models.Model):
-    objects = QuestionManager() 
     title = models.CharField(max_length=125)
     text = models.TextField(default="")
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name="q_to_likes")
     
+    def __unicode__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
 
 class Answer(models.Model):
     text = models.TextField(default="")
-    added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    #added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateField(null=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    #author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User)
     
+    def __unicode__(self):
+        return self.text
