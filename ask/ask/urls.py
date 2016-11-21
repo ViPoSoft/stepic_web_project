bf73fceafcc9489679769f2af2af1aa2ad61e969
ViPoSoft <viposoft@yandex.ru>
2016-11-21 19:37:13 +0200

    var 2.3 v1

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,18 @@
-from django.conf.urls import patterns, include, url
-
+#from django.conf.urls import patterns, include, url
+django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('qa.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
-)
+admin.autodiscover()
+from ask import views
+
+urlpatterns = [
+    url(r'^$', 'views.ktotam'),
+    url(r'^login/', 'views.ktotam'),
+    url(r'^signup/', 'views.ktotam'),
+    url(r'^ask/', 'views.ktotam'),
+    url(r'^popular/', 'views.ktotam'),
+    url(r'^new/', 'views.ktotam'),
+    url(r'^question/', include('qa.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', 'views.nicogodomanet'),                   
+]
