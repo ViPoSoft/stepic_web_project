086290396e00417e71f59e77ea172a1f4724fb07
ViPoSoft <viposoft@yandex.ru>
2016-11-21 21:16:08 +0200

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from qa.views import question
 
 urlpatterns = [
-    url(r'^(?P<num>\d+)/$', question ),
+    url(r'^(?P<num>\d+)/$', question),
 ]
 #urlpatterns = [
     ## druga sproba
