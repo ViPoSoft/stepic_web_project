63aa176cc95444e63f902cf9c95c276490705bbd
ViPoSoft <viposoft@yandex.ru>
2016-11-22 09:49:08 +0200

    les 258 v1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,28 +1,31 @@
+# -*- encoding: utf-8; -*-
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Create your models here.
 class Question(models.Model):
-    title = models.CharField(max_length=125)
-    text = models.TextField(default="")
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
-    
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # http://stackoverflow.com/a/2607848/4241180
+    #author = models.OneToOneField(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes')
+
     def __unicode__(self):
+        """Representation of the instance in admin panel and shell"""
         return self.title
 
-    def get_url(self):
-        return "/question/{}/".format(self.id)
 
 class Answer(models.Model):
-    text = models.TextField(default="")
-    #added_at = models.DateTimeField(auto_now_add=True)
-    added_at = models.DateField(null=True)
-    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-    #author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    author = models.ForeignKey(User)
-    
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, null=True)
+    #question = models.OneToOneField(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __unicode__(self):
-        return self.text
+        return "Answer by {0} to question {1}: {2}...".\
+            format(self.author.username, self.question.id, self.text[:50])
