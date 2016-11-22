28131ed9b1b28ac0f7aa0a20fb303cbd5ffa4127
ViPoSoft <viposoft@yandex.ru>
2016-11-22 09:52:23 +0200

    les 258 v1

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -6,13 +6,6 @@ admin.autodiscover()
 from ask import views
 
 urlpatterns = [
-    url(r'^$', 'views.ktotam'),
-    url(r'^login/', 'views.ktotam'),
-    url(r'^signup/', 'views.ktotam'),
-    url(r'^ask/', 'views.ktotam'),
-    url(r'^popular/', 'views.ktotam'),
-    url(r'^new/', 'views.ktotam'),
-    url(r'^question/', include('qa.urls')),
+    url(r'^', include('qa.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', 'views.nicogodomanet'),                   
-}
+]
