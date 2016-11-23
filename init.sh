1e86fdc06895f2bec6ed586e9c3fe28dd45b43e8
ViPoSoft <viposoft@yandex.ru>
2016-11-23 21:35:27 +0200

    l25 s1

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE dbstep CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "CREATE USER 'vipo'@'localhost' IDENTIFIED BY 'vps23';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'vipo'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
 
 #!/bin/bash ( иногда терминал отрабатывае ошибку на удаление 
 
@@ -5,17 +10,19 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
   sudo rm /etc/nginx/sites-enabled/default
 fi
 # лишнее но пусть будет
-if [ -f /etc/nginx/sites-enabled/default ]; then
-  sudo rm /etc/nginx/sites-enabled/test.conf
-fi
-
+#if [ -f /etc/nginx/sites-enabled/default ]; then
+#  sudo rm /etc/nginx/sites-enabled/test.conf
+#fi
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 # symbolic link to nginx config
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
 sudo /etc/init.d/nginx restart
 
 # symbolic links to gunicorn configs
-sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-
+#sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+#sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
+
+python /home/box/web/ask/manage.py syncdb
