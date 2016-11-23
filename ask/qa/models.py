c64400116388eb701fafd08e16ed775e13892032
ViPoSoft <viposoft@yandex.ru>
2016-11-23 21:03:54 +0200

    l25 s1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,31 +1,32 @@
 # -*- encoding: utf-8; -*-
-from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+#class QuestionManager(models.Manager):
+#    def new(self):
+#        return self.objects.all().order_by('-added_at')
+#    def popular(self):
+#        return self.objects.all().order_by('-rating')
+
 class Question(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=64)
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, null=True)
-    rating = models.IntegerField(blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # http://stackoverflow.com/a/2607848/4241180
-    #author = models.OneToOneField(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='likes')
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default = 0)
+    author = models.ForeignKey(User)  
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    
+    #qobjects = models.Manager()
+    #objects = QuestionManager()
 
     def __unicode__(self):
-        """Representation of the instance in admin panel and shell"""
         return self.title
-
+    
+    #def get_url(self):
+    #    return reverse('questions', kwargs={'id': self.id})
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, null=True)
-    #question = models.OneToOneField(Question, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __unicode__(self):
-        return "Answer by {0} to question {1}: {2}...".\
-            format(self.author.username, self.question.id, self.text[:50])
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  
+    author = models.ForeignKey(User, related_name='answer_user')
