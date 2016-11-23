ce1a803adf6bcde7389d0bfc21afba644bf3ae56
ViPoSoft <viposoft@yandex.ru>
2016-11-23 20:56:29 +0200

    l25 s1

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,13 @@
-#from django.conf.urls import patterns, include, url
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+#from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
-from ask import views
+#from ask import views
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('qa.urls')),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     #url(r'^admin/', include(admin.site.urls)),
 ]
