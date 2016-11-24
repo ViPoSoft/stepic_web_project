8b345f7d916712810314f945359985a672db615c
ViPoSoft <viposoft@yandex.ru>
2016-11-25 01:06:31 +0200

    v26 v51

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,12 +6,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', 'qa.views.newqa'),
+    url(r'^$', 'qa.views.index'),
     url(r'^login/$', 'qa.views.proba'),
     url(r'^signup/$', 'qa.views.proba'),
     url(r'^question/(?P<qid>\d+)/', 'qa.views.question'),
-    url(r'^ask/$', 'qa.views.askfrm'),
-    url(r'^answer/$', 'qa.views.answerfrm'),
+    url(r'^ask/$', 'qa.views.ask'),
+    url(r'^answer/$', 'qa.views.answer'),
     url(r'^popular/$', 'qa.views.popular'),
     url(r'^new/$', 'qa.views.proba'),       
 ]
