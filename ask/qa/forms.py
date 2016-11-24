fc212c583d167f860fc586a0c3733026068fa2fd
ViPoSoft <viposoft@yandex.ru>
2016-11-24 17:20:20 +0200

    l 26 v2

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -6,10 +6,33 @@ class AskForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('title', 'text',)
-    
+
+    def clean(self):
+        pass
+
+    def save(self):
+        question = Question(**self.cleaned_data)
+        question.save()
+        return question
+
+        
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields = ('text','question',)
         
-    
+    def clean_question(self):
+        question_id = self.cleaned_data['question']
+        try:
+            question = Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            question = None
+        return question
+
+    def clean(self):
+        pass
+
+    def save(self):
+        answer = Answer(**self.cleaned_data)
+        answer.save()
+        return answer   
