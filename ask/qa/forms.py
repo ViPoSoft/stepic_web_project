d9a807be95a9f0f96b2f46fee2cf6134fb1e8f6d
ViPoSoft <viposoft@yandex.ru>
2016-11-24 19:18:30 +0200

    Update forms.py

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,38 +1,80 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.forms import ModelForm
 from .models import Question, Answer
+from datetime import datetime
 
-
-class AskForm(forms.ModelForm):
+"""
+class AskForm(ModelForm):
     class Meta:
         model = Question
-        fields = ('title', 'text',)
+        fields = '__all__'
+"""
 
-    def clean(self):
-        pass
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    text = forms.CharField(widget=forms.Textarea)
+   
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.strip() == '':
+            raise forms.ValidationError('Title could not be empty',
+                                        code='validation_error')
+        return title
 
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text.strip() == '':
+            raise forms.ValidationError('Text could not be empty',
+                                        code='validation_error')
+        return text
+            
     def save(self):
+        if self._user.is_anonymous():
+            self.cleaned_data['author_id'] = 1
+        else:
+            self.cleaned_data['author'] = self._user
         question = Question(**self.cleaned_data)
         question.save()
         return question
 
-        
-class AnswerForm(forms.ModelForm):
+"""
+class AnswerForm(ModelForm):
     class Meta:
         model = Answer
-        fields = ('text','question',)
-        
+        fields = '__all__'
+"""
+
+
+class AnswerForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    question = forms.IntegerField(widget=forms.HiddenInput)
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text.strip() == '': 
+            raise forms.ValidationError('Text could not be empty',
+                                        code='validation_error')
+        return text
+
     def clean_question(self):
-        question_id = self.cleaned_data['question']
         try:
-            question = Question.objects.get(id=question_id)
-        except Question.DoesNotExist:
-            question = None
-        return question
+            question_id = int(self.cleaned_data['question'])
+        except ValueError:
+            raise forms.ValidationError('Invalid data',
+                                        code='validation_error')
+        return question_id
 
-    def clean(self):
-        pass
-
+    
     def save(self):
-        answer = Answer(**self.cleaned_data)
+        self.cleaned_data['question'] = get_object_or_404(
+            Question,
+            pk=self.cleaned_data['question'])
+        if self._user.is_anonymous():
+            self.cleaned_data['author_id'] = 1
+        else:
+            self.cleaned_data['author'] = self._user
+	answer = Answer(**self.cleaned_data)
         answer.save()
-        return answer   
+return answer
